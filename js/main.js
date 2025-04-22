387aadf844756561dca84ffc83ee62d43971de27
Gabriel <gabo24jgl@gmail.com>
2025-04-21 18:23:53 -0600

    Animacion y enlaces de contacto

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 // Scroll suave para enlaces del navbar
 document.querySelectorAll('.sidebar a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+  anchor.addEventListener('click', function(e) {
+    const href = this.getAttribute('href');
+    if (href.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
 });
+
 
 //Descargar archivo
 function descargarArchivo(url) {
